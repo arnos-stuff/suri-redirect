f83d5b13f5e8ac12b12572896d12d411249868f1
arnos-stuff <bcda0276@gmail.com>
2023-09-13 07:05:46 +0200

    Committing update to links.json

--- update-links.sh
+++ update-links.sh
@@ -7,5 +7,5 @@
 git clone https://github.com/arnos-stuff/suri-redirect /tmp/suri
 cp links.json /tmp/suri/src/links.json
 cd /tmp/suri
-git add . && git commit -m "Committing update to links.json" && git push origin main
+git add . && git commit -m "Committing update to links.json" && git push origin master
 gh gist edit 7016bacae0337ef54df613cae107fa3f src/links.json
